380e4adc25543c6ea352361c7f98407edc437faa
Jorge <Jorge@DESKTOP-69B394V>
2026-09-17 16:18:17 -0500

    Agregar archivos de proyecto.

---
+++ Inventario_Vidal/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Inventario_Vidal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Inventario_Vidal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Inventario_Vidal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inventario_Vidal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
